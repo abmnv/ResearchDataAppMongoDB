9901d07746f33e84fcc0c89fea60bed7d068f98f
Artem Mamonov <artmnv@gmail.com>
2017-03-27 13:21:36 -0400

    Add heroku support

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
+app.listen(PORT, function(){
+  console.log("Server started on port", PORT);
 });
